87bf155eeecae9afcbfc08f31a3bbed4fc8a4665
Yuan <zhangyuan@users.noreply.github.com>
2019-04-13 17:07:50 +0800

    Add solution for OrderJSONProcessor

--- src/main/scala/datacommunity/datapipeline/OrderJSONProcessor.scala
+++ src/main/scala/datacommunity/datapipeline/OrderJSONProcessor.scala
@@ -1,5 +1,7 @@
 package datacommunity.datapipeline
 
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
 import org.apache.spark.sql.{SaveMode, SparkSession}
 
 object OrderJSONProcessor {
@@ -19,10 +21,40 @@ object OrderJSONProcessor {
   }
 
   def run(spark: SparkSession, inputPath: String, outputPath: String): Unit = {
+
+
+    val schema = StructType(Seq(
+      StructField("timestamp", TimestampType),
+      StructField("payload", StructType(Seq(
+        StructField("orderId", StringType),
+        StructField("items", ArrayType(StructType(Seq(
+          StructField("title", StringType),
+          StructField("quantity", IntegerType)
+        ))))
+      ))
+      )
+    ))
+
+    import spark.implicits._
+
     spark.read
+      .schema(schema)
       .json(inputPath)
+      .withColumn("orderId", $"payload.orderId")
+      .withColumn("item", explode($"payload.items"))
+      .withColumn("Title", $"item.title")
+      .withColumn("Quantity", $"item.quantity")
+      .withColumn("Date", $"timestamp".cast("date"))
+      .select($"Title", $"Quantity", $"Date")
+      .groupBy($"Title", $"Date")
+      .sum("Quantity")
+      .withColumnRenamed("sum(Quantity)", "Total")
+      .select("Date", "Title", "Total")
+      .repartition(1)
       .write
       .mode(SaveMode.Overwrite)
+      .option("header", "true")
       .csv(outputPath)
+
   }
 }
