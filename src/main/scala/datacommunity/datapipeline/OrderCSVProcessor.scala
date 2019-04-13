5d0a5adab43229af9fa5504232b7f3f1a2fbf4e6
Yuan <zhangyuan@users.noreply.github.com>
2019-04-13 17:04:39 +0800

    Add solution for OrderCSVProcessor

--- src/main/scala/datacommunity/datapipeline/OrderCSVProcessor.scala
+++ src/main/scala/datacommunity/datapipeline/OrderCSVProcessor.scala
@@ -1,5 +1,6 @@
 package datacommunity.datapipeline
 
+import org.apache.spark.sql.types._
 import org.apache.spark.sql.{SaveMode, SparkSession}
 
 object OrderCSVProcessor {
@@ -19,11 +20,28 @@ object OrderCSVProcessor {
   }
 
   def run(spark: SparkSession, inputPath: String, outputPath: String): Unit = {
+    val schema = StructType(Seq(
+      StructField("OrderId", StringType),
+      StructField("Title", StringType),
+      StructField("Quantity", IntegerType),
+      StructField("CreateTime", TimestampType)
+    ))
+
+    import spark.implicits._
 
     spark.read
+      .schema(schema).option("header", "true")
       .csv(inputPath)
+      .withColumn("Date", $"CreateTime".cast("date"))
+      .select($"Title", $"Quantity", $"Date")
+      .groupBy($"Title", $"Date")
+      .sum("quantity")
+      .withColumnRenamed("sum(quantity)", "Total")
+      .select("Date", "Title", "Total")
+      .repartition(1)
       .write
       .mode(SaveMode.Overwrite)
+      .option("header", "true")
       .csv(outputPath)
   }
 }
